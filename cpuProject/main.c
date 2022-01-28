e65f2ea821b0b2dddf46192c91aabb3aa4cf7a9c
Jyuri <jeon1310@gmail.com>
2022-01-28 19:36:16 +0900

    초기 커밋.

---
+++ cpuProject/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+// cursor settings function (chohadam 21-03-11)
+void set_cursor(int flag, int size) {
+	CONSOLE_CURSOR_INFO cursor;
+	cursor.bVisible = flag;
+	cursor.dwSize = size;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+}
+void set_console(void) {
+	// set the console size
+	system("mode con:cols=120 lines=31");
+	// hide cursor
+	// set title
+	system("title CPU KINGDOM");
+	// clear console
+	system("cls");
+}
+int main(void) {
+	set_console();
+	pushpull();
+}
